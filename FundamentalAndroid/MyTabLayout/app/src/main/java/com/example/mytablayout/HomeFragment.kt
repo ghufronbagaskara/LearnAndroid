e95010d6e44e225da303dec4b07c0349a513eac5
ghufronbagaskara <ghufronbagas@student.ub.ac.id>
2025-02-06 19:56:58 +0700

    feat@TabLayout: sending data to fragment

--- FundamentalAndroid/MyTabLayout/app/src/main/java/com/example/mytablayout/HomeFragment.kt
+++ FundamentalAndroid/MyTabLayout/app/src/main/java/com/example/mytablayout/HomeFragment.kt
@@ -26,11 +26,16 @@ class HomeFragment : Fragment() {
 
         val tvLabel: TextView = view.findViewById(R.id.section_label)
         val index = arguments?.getInt(ARG_SECTION_NUMBER)
+        val name = arguments?.getString(ARG_NAME)
 
-        tvLabel.text = getString(R.string.content_tab_text, index )
+        var tvText = getString(R.string.content_tab_text, index) + name
+
+        tvLabel.text = tvText
     }
 
     companion object {
         const val ARG_SECTION_NUMBER = "section_number"
+        const val ARG_NAME = "app_name"
+
     }
 }
--- FundamentalAndroid/MyTabLayout/app/src/main/java/com/example/mytablayout/SectionsPagerAdapter.kt
+++ FundamentalAndroid/MyTabLayout/app/src/main/java/com/example/mytablayout/SectionsPagerAdapter.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class SectionsPagerAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity) {
+    var appName: String = ""
+
     override fun getItemCount(): Int {
         return 3
     }
@@ -14,8 +16,10 @@ class SectionsPagerAdapter(activity: AppCompatActivity): FragmentStateAdapter(ac
         val fragment = HomeFragment()
         fragment.arguments = Bundle().apply {
             putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
+            putString(HomeFragment.ARG_NAME, appName)
         }
         return fragment
     }
+
 
 }
